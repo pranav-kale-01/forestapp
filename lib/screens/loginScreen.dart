3b3602a9a29822bbb50933f301c888ad794064d5
PranayChavhan <pranay21chavhan@outlook.com>
2023-04-30 23:23:34 +0530

    login screen added

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -27,80 +27,81 @@ class _LoginScreenState extends State<LoginScreen>{
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            // Container(
-            //   height: _headerHeight,
-            //   child: HeaderWidget(_headerHeight, true, Icons.login_rounded), //let's create a common header widget
-            // ),
-            SafeArea(
-              child: Container(
-                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),// This will be the login form
-                  child: Column(
-                    children: [
-                      const Text(
-                        'Signin into your account',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      const SizedBox(height: 30.0),
-                      Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Container(
-                                decoration: ThemeHelper().inputBoxDecorationShaddow(),
-                                child: TextField(
-                                  decoration: ThemeHelper().textInputDecoration('User Name', 'Enter your user name'),
-                                ),
-                              ),
-                              const SizedBox(height: 30.0),
-                              Container(
-                                decoration: ThemeHelper().inputBoxDecorationShaddow(),
-                                child: TextField(
-                                  obscureText: true,
-                                  decoration: ThemeHelper().textInputDecoration('Password', 'Enter your password'),
-                                ),
-                              ),
-                              const SizedBox(height: 15.0),
-                              Container(
-                                margin: const EdgeInsets.fromLTRB(10,0,10,20),
-                                alignment: Alignment.topRight,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    // Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), );
-                                  },
-                                  child: const Text( "Forgot your password?", style: TextStyle( color: Colors.grey, ),
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                decoration: ThemeHelper().buttonBoxDecoration(context),
-                                child: ElevatedButton(
-                                  style: ThemeHelper().buttonStyle(),
-                                  child: Padding(
-                                    padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                                    child: Text('Sign In'.toUpperCase(), style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
-                                  ),
-                                  onPressed: (){
-                                    //After successful login we will redirect to profile page. Let's create profile page now
-                                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-                                  },
-                                ),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.fromLTRB(10,20,10,20),
-                                //child: Text('Don\'t have an account? Create'),
+        child: Center(
+          child: Column(
 
-                              ),
-                            ],
-                          )
-                      ),
-                    ],
-                  )
+            children: [
+
+              SafeArea(
+
+                child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 200, 20, 10),
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 10),// This will be the login form
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Signin into your account',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const SizedBox(height: 30.0),
+                        Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                Container(
+                                  decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                                  child: TextField(
+                                    decoration: ThemeHelper().textInputDecoration('User Name', 'Enter your user name'),
+                                  ),
+                                ),
+                                const SizedBox(height: 30.0),
+                                Container(
+                                  decoration: ThemeHelper().inputBoxDecorationShaddow(),
+                                  child: TextField(
+                                    obscureText: true,
+                                    decoration: ThemeHelper().textInputDecoration('Password', 'Enter your password'),
+                                  ),
+                                ),
+                                const SizedBox(height: 15.0),
+                                Container(
+                                  margin: const EdgeInsets.fromLTRB(10,0,10,20),
+                                  alignment: Alignment.topRight,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      // Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), );
+                                    },
+                                    child: const Text( "Forgot your password?", style: TextStyle( color: Colors.grey, ),
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  decoration: ThemeHelper().buttonBoxDecoration(context),
+                                  child: ElevatedButton(
+                                    style: ThemeHelper().buttonStyle(),
+                                    child: Padding(
+                                      padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                      child: Text('Sign In'.toUpperCase(), style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+                                    ),
+                                    onPressed: (){
+                                      //After successful login we will redirect to profile page. Let's create profile page now
+                                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+                                    },
+                                  ),
+                                ),
+                                Container(
+                                  margin: const EdgeInsets.fromLTRB(10,20,10,20),
+                                  //child: Text('Don\'t have an account? Create'),
+
+                                ),
+                              ],
+                            )
+                        ),
+                      ],
+                    )
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
